1cde53a508a572b52c716acf7b0efaa3b90f126b
Sinduja Raghavendra <sindujak29@gmail.com>
2020-06-04 18:53:49 -0700

    [doc]: Update README
    Details:
    1. Update readme
        * Find a Venue
        * Find an Artist
        * Search Venue in the Venue home page
        * Search Artist in the Artist home page
        * Post a Venue
    
    Resolves:
     * How to post a venue
     * Intro about Venue list and search with the keyword
     * Intro about Artist and search with keyword
    
    Tests:
     * By running App on localhost
    
    Signed-off-by: Sinduja Raghavendra <sindujak29@gmail.com>

--- forms.py
+++ forms.py
@@ -177,10 +177,10 @@ class ArtistForm(FlaskForm):
 
 class ShowForm(FlaskForm):
     artist_id = StringField(
-        'artist_id'
+        'artist_id', validators=[DataRequired()]
     )
     venue_id = StringField(
-        'venue_id'
+        'venue_id', validators=[DataRequired()]
     )
     start_time = DateTimeField(
         'start_time',
